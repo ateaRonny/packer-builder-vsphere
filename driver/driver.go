919a0307cf6003149b3ce1d9c61540ee83a97ccb
Ronny Talberg <ronny.talberg@atea.no>
2019-08-12 08:15:16 +0200

    Session based vSphere authentication. Funcs from vSphere Terraform provider.

--- driver/driver.go
+++ driver/driver.go
@@ -2,15 +2,24 @@ package driver
 
 import (
 	"context"
+	"crypto/sha1"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/mitchellh/go-homedir"
+
 	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/find"
 	"github.com/vmware/govmomi/object"
 	"github.com/vmware/govmomi/session"
 	"github.com/vmware/govmomi/vim25"
 	"github.com/vmware/govmomi/vim25/soap"
-	"net/url"
-	"time"
+	"github.com/vmware/govmomi/vim25/types"
 )
 
 type Driver struct {
@@ -26,7 +35,243 @@ type ConnectConfig struct {
 	Password           string
 	InsecureConnection bool
 	Datacenter         string
+	KeepAlive          int
+	Persist            bool
+	VimSessionPath     string
 }
+
+// Define default timeout for vsphere API
+const defaultAPITimeout = time.Minute * 5
+
+// **************************************
+// Added from terraform provider and modified
+// ************************************
+
+// SavedVimSessionOrNew either loads a saved SOAP session from disk, or creates
+// a new one.
+func (c *ConnectConfig) SavedVimSessionOrNew(u *url.URL) (*govmomi.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultAPITimeout)
+	defer cancel()
+
+	client, err := c.LoadVimClient()
+	if err != nil {
+		return nil, fmt.Errorf("error trying to load vSphere SOAP session from disk: %s", err)
+	}
+	if client == nil {
+		log.Printf("[DEBUG] Creating new SOAP API session on endpoint %s", c.VCenterServer)
+		client, err = newClientWithKeepAlive(ctx, u, c.InsecureConnection, c.KeepAlive)
+		if err != nil {
+			return nil, fmt.Errorf("error setting up new vSphere SOAP client: %s", err)
+		}
+		log.Println("[DEBUG] SOAP API session creation successful")
+	}
+	return client, nil
+}
+
+func newClientWithKeepAlive(ctx context.Context, u *url.URL, insecure bool, keepAlive int) (*govmomi.Client, error) {
+	soapClient := soap.NewClient(u, insecure)
+	vimClient, err := vim25.NewClient(ctx, soapClient)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &govmomi.Client{
+		Client:         vimClient,
+		SessionManager: session.NewManager(vimClient),
+	}
+
+	k := session.KeepAlive(c.Client.RoundTripper, time.Duration(keepAlive)*time.Minute)
+	c.Client.RoundTripper = k
+
+	// Only login if the URL contains user information.
+	if u.User != nil {
+		err = c.Login(ctx, u.User)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return c, nil
+}
+
+// LoadVimClient loads a saved vSphere SOAP API session from disk, previously
+// saved by SaveVimClient, checking it for validity before returning it. A nil
+// client means that the session is no longer valid and should be created from
+// scratch.
+//
+// Note the logic in this function has been largely adapted from govc and is
+// designed to be compatible with it - if a session has already been saved with
+// govc, Terraform will attempt to use that session first.
+func (c *ConnectConfig) LoadVimClient() (*govmomi.Client, error) {
+	client := new(vim25.Client)
+	ok, err := c.restoreVimClient(client)
+	if err != nil {
+		return nil, err
+	}
+
+	if !ok || !client.Valid() {
+		log.Println("[DEBUG] Cached SOAP client session data not valid or persistence not enabled, new session necessary")
+		return nil, nil
+	}
+
+	m := session.NewManager(client)
+	u, err := m.UserSession(context.TODO())
+	if err != nil {
+		if soap.IsSoapFault(err) {
+			fault := soap.ToSoapFault(err).VimFault()
+			// If the PropertyCollector is not found, the saved session for this URL is not valid
+			if _, ok := fault.(types.ManagedObjectNotFound); ok {
+				log.Println("[DEBUG] Cached SOAP client session missing property collector, new session necessary")
+				return nil, nil
+			}
+		}
+
+		return nil, err
+	}
+
+	// If the session is nil, the client is not authenticated
+	if u == nil {
+		log.Println("[DEBUG] Unauthenticated session, new session necessary")
+		return nil, nil
+	}
+
+	log.Println("[DEBUG] Cached SOAP client session loaded successfully")
+	return &govmomi.Client{
+		Client:         client,
+		SessionManager: m,
+	}, nil
+}
+
+// restoreVimClient loads the saved session from disk. Note that this is a helper
+// function to LoadVimClient and should not be called directly.
+func (c *ConnectConfig) restoreVimClient(client *vim25.Client) (bool, error) {
+	if !c.Persist {
+		return false, nil
+	}
+
+	p, err := c.vimSessionFile()
+	if err != nil {
+		return false, err
+	}
+	log.Printf("[DEBUG] Attempting to locate SOAP client session data in %q", p)
+	f, err := os.Open(p)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Printf("[DEBUG] SOAP client session data not found in %q", p)
+			return false, nil
+		}
+
+		return false, err
+	}
+
+	defer func() {
+		if err = f.Close(); err != nil {
+			log.Printf("[DEBUG] Error closing SOAP client session file %q: %s", p, err)
+		}
+	}()
+
+	dec := json.NewDecoder(f)
+	err = dec.Decode(client)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// SaveVimClient saves a client to the supplied path. This facilitates re-use of
+// the session at a later date.
+//
+// Note the logic in this function has been largely adapted from govc and is
+// designed to be compatible with it.
+func (c *ConnectConfig) SaveVimClient(client *govmomi.Client) error {
+	if !c.Persist {
+		return nil
+	}
+
+	p, err := c.vimSessionFile()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("[DEBUG] Will persist SOAP client session data to %q", p)
+	err = os.MkdirAll(filepath.Dir(p), 0700)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.OpenFile(p, os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err = f.Close(); err != nil {
+			log.Printf("[DEBUG] Error closing SOAP client session file %q: %s", p, err)
+		}
+	}()
+
+	err = json.NewEncoder(f).Encode(client.Client)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// vimSessionFile is takes the session file name generated by sessionFile and
+// then prefixes the SOAP client session path to it.
+func (c *ConnectConfig) vimSessionFile() (string, error) {
+	p, err := c.sessionFile()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(c.VimSessionPath, p), nil
+}
+
+// sessionFile is a helper that generates a unique hash of the client's URL
+// to use as the session file name.
+//
+// This is the same logic used as part of govmomi and is designed to be
+// consistent so that sessions can be shared if possible between both tools.
+func (c *ConnectConfig) sessionFile() (string, error) {
+	u, err := c.vimURLWithoutPassword()
+	if err != nil {
+		return "", err
+	}
+
+	// Key session file off of full URI and insecure setting.
+	// Hash key to get a predictable, canonical format.
+	key := fmt.Sprintf("%s#insecure=%t", u.String(), c.InsecureConnection)
+	name := fmt.Sprintf("%040x", sha1.Sum([]byte(key)))
+	return name, nil
+}
+
+func (c *ConnectConfig) vimURLWithoutPassword() (*url.URL, error) {
+	u, err := c.vimURL()
+	if err != nil {
+		return nil, err
+	}
+	withoutCredentials := u
+	withoutCredentials.User = url.User(u.User.Username())
+	return withoutCredentials, nil
+}
+
+// vimURL returns a URL to pass to the VIM SOAP client.
+func (c *ConnectConfig) vimURL() (*url.URL, error) {
+	u, err := url.Parse("https://" + c.VCenterServer + "/sdk")
+	if err != nil {
+		return nil, fmt.Errorf("Error parse url: %s", err)
+	}
+
+	u.User = url.UserPassword(c.Username, c.Password)
+
+	return u, nil
+}
+
+// **************************************
+// End: Added from terraform provider
+// ************************************
 
 func NewDriver(config *ConnectConfig) (*Driver, error) {
 	ctx := context.TODO()
@@ -38,23 +283,26 @@ func NewDriver(config *ConnectConfig) (*Driver, error) {
 	credentials := url.UserPassword(config.Username, config.Password)
 	vcenterUrl.User = credentials
 
-	soapClient := soap.NewClient(vcenterUrl, config.InsecureConnection)
-	vimClient, err := vim25.NewClient(ctx, soapClient)
+	//TODO: Create logic to set vim session path only if missing from config file
+	userHomeDir, err := homedir.Dir()
 	if err != nil {
 		return nil, err
 	}
 
-	vimClient.RoundTripper = session.KeepAlive(vimClient.RoundTripper, 10*time.Minute)
-	client := &govmomi.Client{
-		Client:         vimClient,
-		SessionManager: session.NewManager(vimClient),
-	}
+	config.VimSessionPath = filepath.Join(userHomeDir, ".govmomi", "sessions")
 
-	err = client.SessionManager.Login(ctx, credentials)
+	client := new(govmomi.Client)
+	client, err = config.SavedVimSessionOrNew(vcenterUrl)
 	if err != nil {
 		return nil, err
 	}
 
+	if config.Persist == true {
+		err = config.SaveVimClient(client)
+		if err != nil {
+			return nil, err
+		}
+	}
 	finder := find.NewFinder(client.Client, false)
 	datacenter, err := finder.DatacenterOrDefault(ctx, config.Datacenter)
 	if err != nil {
